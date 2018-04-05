e59bff076d6cd482f81283e05e87be8404b584b4
schomy <sebastien.chomy@gmail.com>
2018-04-05 11:34:26 +0200

    [COMPOSER] Add DoctrineFixturesBundle

--- config/bundles.php
+++ config/bundles.php
@@ -3,6 +3,7 @@
 return [
     Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
