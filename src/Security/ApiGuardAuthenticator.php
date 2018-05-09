d5c843d895a00a53b593a48396ec199f844e6aad
schomy <sebastien.chomy@gmail.com>
2018-05-09 11:07:27 +0200

    ApiGuardAuthenticator Update

--- src/Security/ApiGuardAuthenticator.php
+++ src/Security/ApiGuardAuthenticator.php
@@ -25,17 +25,7 @@ use Symfony\Component\Security\Guard\AbstractGuardAuthenticator;
 class ApiGuardAuthenticator extends AbstractGuardAuthenticator
 {
     /**
-     * Returns a response that directs the user to authenticate.
-     *
-     * This is called when an anonymous request accesses a resource that
-     * requires authentication. The job of this method is to return some
-     * response that "helps" the user start into the authentication process.
-     *
-     * Examples:
-     *  A) For a form login, you might redirect to the login page
-     *      return new RedirectResponse('/login');
-     *  B) For an API token authentication system, you return a 401 response
-     *      return new Response('Auth header required', 401);
+     * @inheritdoc
      *
      * @param Request                 $request       The request that resulted in an AuthenticationException
      * @param AuthenticationException $authException The exception that started the authentication process
@@ -44,18 +34,11 @@ class ApiGuardAuthenticator extends AbstractGuardAuthenticator
      */
     public function start(Request $request, AuthenticationException $authException = null)
     {
-        $data = [
-            // you might translate this message
-            'message' => 'Authentication Required'
-        ];
-
-        return new JsonResponse($data, Response::HTTP_UNAUTHORIZED);
+        return new JsonResponse(['message' => 'Authentication Required'], Response::HTTP_UNAUTHORIZED);
     }
 
     /**
-     * Does the authenticator support the given Request?
-     *
-     * If this returns false, the authenticator will be skipped.
+     * @inheritdoc
      *
      * @param Request $request
      *
@@ -63,100 +46,52 @@ class ApiGuardAuthenticator extends AbstractGuardAuthenticator
      */
     public function supports(Request $request): bool
     {
-        return $request->headers->has('X-AUTH-TOKEN');
+        return $request->headers->has('X-API-TOKEN');
     }
 
     /**
-     * Get the authentication credentials from the request and return them
-     * as any type (e.g. an associate array).
-     *
-     * Whatever value you return here will be passed to getUser() and checkCredentials()
-     *
-     * For example, for a form login, you might:
-     *
-     *      return array(
-     *          'username' => $request->request->get('_username'),
-     *          'password' => $request->request->get('_password'),
-     *      );
-     *
-     * Or for an API token that's on a header, you might use:
-     *
-     *      return array('api_key' => $request->headers->get('X-API-TOKEN'));
+     * @inheritdoc
      *
      * @param Request $request
      *
      * @return mixed Any non-null value
      *
-     * @throws \UnexpectedValueException If null is returned
      */
     public function getCredentials(Request $request): array
     {
-        return ['token' => $request->headers->get('X-AUTH-TOKEN')];
+        return ['api_key' => $request->headers->get('X-API-TOKEN')];
     }
 
     /**
-     * Return a UserInterface object based on the credentials.
-     *
-     * The *credentials* are the return value from getCredentials()
-     *
-     * You may throw an AuthenticationException if you wish. If you return
-     * null, then a UsernameNotFoundException is thrown for you.
+     * @inheritdoc
      *
      * @param mixed                 $credentials
      * @param UserProviderInterface $userProvider
      *
-     * @throws AuthenticationException
-     *
      * @return UserInterface|null
+     *
+     * @throws AuthenticationException
      */
     public function getUser($credentials, UserProviderInterface $userProvider)
     {
-        $apiKey = $credentials['token'];
-
-        if (null === $apiKey) {
-            return;
-        }
-
-        // if a User object, checkCredentials() is called
-        return $user = $userProvider->loadUserByUsername($apiKey);
+        return $userProvider->loadUserByUsername($credentials['api_key']);
     }
 
     /**
-     * Returns true if the credentials are valid.
-     *
-     * If any value other than true is returned, authentication will
-     * fail. You may also throw an AuthenticationException if you wish
-     * to cause authentication to fail.
-     *
-     * The *credentials* are the return value from getCredentials()
+     * @inheritdoc
      *
      * @param mixed         $credentials
      * @param UserInterface $user
      *
      * @return bool
-     *
-     * @throws AuthenticationException
      */
     public function checkCredentials($credentials, UserInterface $user)
     {
-        if ($credentials['token'] === $user->getApiKey()) {
-            return true;
-        }
-        // check credentials - e.g. make sure the password is valid
-        // no credential check is needed in this case
-
-        // return true to cause authentication success
-        return false;
+        return true;
     }
 
     /**
-     * Called when authentication executed, but failed (e.g. wrong username password).
-     *
-     * This should return the Response sent back to the user, like a
-     * RedirectResponse to the login page or a 403 response.
-     *
-     * If you return null, the request will continue, but the user will
-     * not be authenticated. This is probably not what you want to do.
+     * @inheritdoc
      *
      * @param Request                 $request
      * @param AuthenticationException $exception
@@ -165,24 +100,14 @@ class ApiGuardAuthenticator extends AbstractGuardAuthenticator
      */
     public function onAuthenticationFailure(Request $request, AuthenticationException $exception)
     {
-        $data = [
-            'message' => strtr($exception->getMessageKey(), $exception->getMessageData())
-
-            // or to translate this message
-            // $this->translator->trans($exception->getMessageKey(), $exception->getMessageData())
-        ];
-
-        return new JsonResponse($data, Response::HTTP_FORBIDDEN);
+        return new JsonResponse(
+            ['message' => strtr($exception->getMessageKey(), $exception->getMessageData())],
+            Response::HTTP_FORBIDDEN
+        );
     }
 
     /**
-     * Called when authentication executed and was successful!
-     *
-     * This should return the Response sent back to the user, like a
-     * RedirectResponse to the last page they visited.
-     *
-     * If you return null, the current request will continue, and the user
-     * will be authenticated. This makes sense, for example, with an API.
+     * @inheritdoc
      *
      * @param Request        $request
      * @param TokenInterface $token
@@ -192,21 +117,11 @@ class ApiGuardAuthenticator extends AbstractGuardAuthenticator
      */
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey)
     {
-        // on success, let the request continue
         return null;
     }
 
     /**
-     * Does this method support remember me cookies?
-     *
-     * Remember me cookie will be set if *all* of the following are met:
-     *  A) This method returns true
-     *  B) The remember_me key under your firewall is configured
-     *  C) The "remember me" functionality is activated. This is usually
-     *      done by having a _remember_me checkbox in your form, but
-     *      can be configured by the "always_remember_me" and "remember_me_parameter"
-     *      parameters under the "remember_me" firewall key
-     *  D) The onAuthenticationSuccess method returns a Response object
+     * @inheritdoc
      *
      * @return bool
      */
