dcab14ba215f9a7d9c217087145f50c352d59be3
schomy <sebastien.chomy@gmail.com>
2018-05-28 13:51:48 +0200

    #32-JSON-Authentication-Endpoint-in-APIDoc

--- src/Controller/Api/SecurityController.php
+++ src/Controller/Api/SecurityController.php
@@ -8,8 +8,16 @@
 
 namespace App\Controller\Api;
 
-use Symfony\Component\Routing\Annotation\Route;
+use App\Manager\UserManager;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\Controller\Annotations\QueryParam;
+use FOS\RestBundle\Request\ParamFetcherInterface;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Swagger\Annotations as SWG;
 
 /**
  * Class SecurityController
@@ -18,11 +26,57 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
  */
 class SecurityController extends Controller
 {
-
     /**
-     * @Route("/apilogin")
+     * @Rest\Post(
+     *     path="/api/login",
+     *     name="app_api_security_login"
+     *     )
+     *
+     * @QueryParam(
+     *     name="username",
+     *     requirements="\w+",
+     *     description="Your username"
+     * )
+     * @QueryParam(
+     *     name="password",
+     *     requirements="\w+",
+     *     description="A password"
+     * )
+     *
+     * @SWG\Response(
+     *     response="200",
+     *     description="Returned the token"
+     * )
+     * @SWG\Response(
+     *     response="403",
+     *     description="Returned when there is bad credentials"
+     * )
+     *
+     * @param JWTTokenManagerInterface     $JWTTokenManager
+     * @param ParamFetcherInterface        $paramFetcher
+     * @param UserManager                  $userManager
+     * @param UserPasswordEncoderInterface $userPasswordEncoder
+     *
+     * @return JsonResponse
+     *
+     * @throws AccessDeniedHttpException
+     * @throws \Doctrine\ORM\NonUniqueResultException
      */
-    public function login()
-    {
+    public function apiLoginAction(
+        JWTTokenManagerInterface $JWTTokenManager,
+        ParamFetcherInterface $paramFetcher,
+        UserManager $userManager,
+        UserPasswordEncoderInterface $userPasswordEncoder
+    ) {
+        $username = $paramFetcher->get('username');
+        $user = $userManager->loadUserByUsername($username);
+
+        if (null !== $user) {
+            if ($userPasswordEncoder->isPasswordValid($user, $paramFetcher->get('password'))) {
+                return new JsonResponse(['token' => $JWTTokenManager->create($user)]);
+            }
+        }
+
+        throw new AccessDeniedHttpException('Bad credentials');
     }
 }
